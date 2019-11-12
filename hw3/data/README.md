7191cbc99767111b60490f8f83880cf14ed7f432
Jason Fan <jasonfan@cs.umd.edu>
2019-11-12 17:33:04 -0500

    finished sbm:

---
+++ hw3/data/README.md
@@ -0,0 +1,6 @@
+#Data from: Kefi et al. PLOS Bio. 2016
+
+How structured is the entangled bank? The surprisingly simple organization of multiplex ecological networks leads to increased persistence and resilience
+
+
+
--- hw3/sbm.py
+++ hw3/sbm.py
@@ -1,8 +1,8 @@
 #import click
 import numpy as np 
-from scipy.special import expit as sigmoid
 
 def gen_sbm(n_per_clust, k_clusters):
+    from scipy.special import expit as sigmoid
     n_nodes = n_per_clust * k_clusters
     nodes = np.arange(n_nodes)
     node_labels = np.sort(nodes % k_clusters)
@@ -13,8 +13,6 @@ def gen_sbm(n_per_clust, k_clusters):
     clust_ps = clust_ps + clust_ps.T
     clust_ps = sigmoid(clust_ps)
     clust_ps[np.eye(k_clusters, dtype=bool)] = 0.9
-
-    #print(clust_ps)
 
     A = np.random.uniform(0, 1, (n_nodes, n_nodes))
     for i in range(n_nodes):
@@ -28,101 +26,101 @@ def gen_sbm(n_per_clust, k_clusters):
     
         
     return A, node_labels, clust_ps
-            
-
-# @click.command()
-# @click.option('-n', '--n_per_clust', type=int)
-# @click.option('-k', '--k_clusters', type=int)
-# @click.option('-oa', '--out_adj')
-# @click.option('-on', '--out_nodes')
-# @click.option('-oc', '--out_ps')
-# def cli(n_per_clust, k_clusters, out_adj, out_nodes, out_ps):
-#     gen_sbm(n_per_clust, k_clusters)
-
-
-
-
-# if __name__ == "__main__":
-#     cli()
 
 class SBM(object):
-    def __init__(self, A, k, tol=1e-6, max_iters=20, max_e_iters=100):
+    def __init__(self, A, k, tol=1e-6, max_iters=20, max_e_iters=100, init='spectral'):
         assert(np.allclose(np.diag(A), 0))
+        assert(np.allclose(A, A.T))
         self.tol = tol
         self.A = A
         self.k = k
         self.n = len(A)
         self.max_iters = max_iters
         self.max_e_iters = max_e_iters
-        
-        # cluster linkage probabality
-        self.theta = np.random.uniform(high=1, size=(self.k, self.k))
-        #self.theta += self.theta.T
 
-        # cluster prior. (probability any node is assigned a cluster)
-        self.alpha = np.random.uniform(size=self.k)
-        self.alpha = self.alpha / np.sum(self.alpha)  
-
-        # per node cluster probabality
-        self.gamma = np.random.uniform(size=(self.n, self.k))
-        self.gamma = self.gamma / np.sum(self.gamma, axis=1, keepdims=True)
-
-        # Zs
-        self.Z = np.random.randint(low=0, high=self.k, size=self.n)
+        # Set initial Z assignment
+        if init is 'random':
+            self.random_init()
+        elif init == 'spectral':
+            self.spectral_init()
+        else:
+            raise NotImplementedError
+        self.init_from_Z()
 
         assert(np.allclose(self.alpha.sum(), 1))
         assert(np.allclose(np.sum(self.gamma, axis=1), 1))
         assert(all(self.Z < k))
+    
+    def spectral_init(self):
+        from sklearn.cluster import SpectralClustering
+        spectral_clust = SpectralClustering(n_clusters=self.k, affinity='precomputed')
+        spectral_clust.fit(self.A)
+        self.Z = spectral_clust.fit_predict(self.A)
+    
+    def random_init(self):
+        self.Z = np.random.randint(low=0, high=self.k, size=self.n)
+    
+    def init_from_Z(self, init_noise=1e-4):
+        # Initialize parameters from cluster assignment labels.
+        self.gamma = np.zeros((self.n, self.k))
+        self.gamma[range(self.n), self.Z] = 1
+        self.gamma = self.gamma + init_noise
+        self.gamma = self.gamma / np.sum(self.gamma, axis=1, keepdims=True)
 
-    def fit(self, verbose=True):
-        self.alpha = self._M_alpha(self.gamma, self.n)
+        self.alpha = self._M_alpha(self.gamma)
         self.theta = self._M_theta(self.A, self.gamma)
+
+    def fit(self, verbose=False, verbose_E=False, warn=True):
+        NLL_old  = self.NLL()
         for i in range(self.max_iters):
-            if verbose: print(i)
-            self.gamma = self._E(verbose)
-            self.alpha = self._M_alpha(self.gamma, self.n)
+            self.gamma = self._E_fast(verbose_E)
+            self.alpha = self._M_alpha(self.gamma)
             self.theta = self._M_theta(self.A, self.gamma)
-    
+            self.Z = self.MAP_Z()
+            NLL_new = self.NLL()
+
+            if verbose: print('[ITER]', i, 'NLL=%f' % self.NLL())
+
+            if NLL_new <= NLL_old and np.abs(NLL_old - NLL_new) < self.tol:
+                if verbose: print("\tConverged...")
+                break
+            NLL_old = NLL_new
+        self.iters_ran = i + 1
         return self
-    
-    def _E(self, verbose=True):
+
+    def _E_fast(self, verbose=True):
+        # Matrix multiplication for the fixed point is necessary for 
+        # a sane, reasonably fast model. 
+        # https://github.com/cran/blockmodels/blob/eb543aba11cb0ece159fa7dc01d1eb8bb9d32ed2/src/models/bernoulli.h#L89
         rel_err = 1
         tau_old = self.gamma.copy()
         for it in range(self.max_e_iters):
+
             tau_new = tau_old.copy()
-            for i in range(self.n):
-                for q in range(self.k):
-                    tau_new[i,q] = self.iter_fixed_point(i, q, tau_old)
+            log_1 = np.linalg.multi_dot([self.A, tau_new, np.log(self.theta) + np.log(self.theta.T)])
+            Y_not = 1 - self.A - np.eye(len(self.A))
+            log_2 = np.linalg.multi_dot([Y_not, tau_new, np.log(1 - self.theta) + np.log(1 - self.theta.T)])
+            tau_new_log = np.log(self.alpha) + log_1 + log_2
             
-            #print(tau_new)
+            # Clipping and manipulation to avoid overflow/underflow
+            tau_new_log = tau_new_log - np.max(tau_new_log, axis=1, keepdims=True)
+            tau_new = np.exp(tau_new_log) + 1e-9
+
+            if np.any(np.isnan(tau_new)): 
+                assert(False)
             tau_new = tau_new / np.sum(tau_new, axis=1, keepdims=True)
             rel_err = np.linalg.norm(tau_new - tau_old)
             if verbose and (it + 1) % 10 == 0: print("\t[%d] E rel_err: %f" %(it,rel_err))
-            if rel_err < self.tol: break
+
             tau_old = tau_new
+            if rel_err < self.tol: 
+                break
         if verbose: print("\t[%d] E rel_err: %f" %(it,rel_err))
         return tau_old
 
-
-    def iter_fixed_point(self, i, q, tau_old):
-        prod = self.alpha[q]
-        for j in range(self.n):
-            if i == j: continue
-            for l in range(self.k):
-                y_ij = self.A[i, j]
-                theta_ql = self.theta[q,l]
-                theta_lq = self.theta[l,q]
-                if y_ij == 1:
-                    prod *= np.power(theta_ql*theta_lq, tau_old[j, l])
-                else:
-                    prod *= np.power((1 - theta_ql)*(1-theta_lq), tau_old[j, l])
-                assert(~np.isnan(prod))
-
-        return prod
-    
     @staticmethod
-    def _M_alpha(gamma, n):
-        alpha = np.sum(gamma, axis=0) / n
+    def _M_alpha(gamma):
+        alpha = np.sum(gamma, axis=0) / len(gamma)
         assert(np.allclose(np.sum(alpha), 1))
         assert(alpha.shape == (gamma.shape[1],))
         return alpha
@@ -145,7 +143,34 @@ class SBM(object):
                 theta[q,r] = sum_yqr / sum_qr
                 theta[r,q] = sum_yqr / sum_qr
         return theta
+    
+    def NLL(self):
+        Z = self.Z
+        alphas = self.alpha[Z]
+        prior_logL = np.log(alphas)
+        prior_logL = np.sum(prior_logL)
 
+        logL = 0
+        for i in range(self.n):
+            for j in range(i + 1, self.n):
+                y_ij = self.A[i, j]
+                p_ij = self.theta[Z[i], Z[j]]
+                if y_ij == 1:
+                    logL += np.log(p_ij)
+                else:
+                    logL += np.log(1 - p_ij)
 
-    def L(self):
-        return 0
+        L = - (prior_logL + logL)
+        return L
+
+    def MAP_Z(self):
+        return np.argmax(self.gamma, axis=1)
+
+    def ICL(self):
+        # maximize for model selection criterion
+        ICL_a = -self.NLL()
+        ICL_b = self.k*(self.k +1)  / 2
+        ICL_b *= np.log(self.n * (self.n-1))
+        ICL_c = (self.k - 1) * np.log(self.n)
+        return ICL_a - 0.5 * (ICL_b - ICL_c)
+        
